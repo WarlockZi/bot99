13d784f3ebb9c92e1a8808e7a6af6ee0ffc95383
Vitaliy <vvoronik@yandex.ru>
2023-08-21 16:10:03 +0300

    fix htaccess

--- public/index.php
+++ public/index.php
@@ -8,7 +8,6 @@ define('LARAVEL_START', microtime(true));
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
