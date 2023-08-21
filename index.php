fa1b460f7d329677a23cdd18a17fc73b54070bdd
Vitaliy <vvoronik@yandex.ru>
2023-08-21 16:59:40 +0300

    fix htaccess

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 define('ROOT', __DIR__);
 var_dump(ROOT);
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 // exit(ROOT);
 // header('Location: backend/public/index.php');
